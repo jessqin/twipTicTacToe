b2a103a1ef6e944ce13d8cd974cf095f13e5220d
Jessica Qin <jessicaq319@gmail.com>
2016-12-23 20:33:15 -0500

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,90 +8,207 @@ public class Main {
 
         Point cPointX = new Point();
         Point cPointY = new Point();
-        String player = "O";
-        String comp = "X";
-        //int row;
-
 
         String board[][] = {{"---", "---", "---"},
                 {"---", "---", "---"},
                 {"---", "---", "---"}};
 
-
-
-//        for (int row = 0; row < board.length; row++)
-//        {
-//            for (int col = 0; col < board[row].length; col++)
-//            {
-//                System.out.print(board[row][col] + "\t");
-//            }
-//            System.out.println("\n");
-//        }
-
-
-        System.out.println("Play against a computer or player?");
-        Scanner keyIn = new Scanner(System.in);
-        String cp = keyIn.next();
-
-        System.out.println("You will be O and the computer will be X. Your turn is first.");
-
-        if (cp.equalsIgnoreCase("computer"))
+        outerloop1:
+        while (true)
         {
 
+            System.out.println("Play against a computer or player?");
+            Scanner keyIn = new Scanner(System.in);
+            String cp = keyIn.next();
 
-            for (int i = 0; i < 9; i++)
+            outerloop:
+            if (cp.equalsIgnoreCase("computer"))
             {
+                System.out.println("You will be O and the computer will be X. Your turn is first.");
 
-                System.out.println("Please enter a point");
-
-                Scanner kI = new Scanner(System.in);
-                String pChoice = kI.next();
-
-
-                String sVal [] = pChoice.split(",");
-                int iValRow = Integer.parseInt(sVal[0]);
-                int iValCol = Integer.parseInt(sVal[1]);
-
-
-                System.out.println( "Your point is: " + iValRow + "," + iValCol);
-
-                int rowTest = cPointX.setPoint();
-                int colTest = cPointY.setPoint();
-
-                System.out.println("The computer's point is: " + rowTest + "," + colTest);
-
-                board[iValRow][iValCol] = " O";
-
-                if (board[rowTest][colTest].equalsIgnoreCase(" X") && board[rowTest][colTest].equalsIgnoreCase(" O"))
+                for (int i = 0; i < 9; i++)
                 {
-                    for (int k = 0; k < 10000; k++)
+
+                    System.out.println("Please enter a point");
+
+                    Scanner kI = new Scanner(System.in);
+                    String pChoice = kI.next();
+
+
+                    String sVal [] = pChoice.split(",");
+                    int iValRow = Integer.parseInt(sVal[0]);
+                    int iValCol = Integer.parseInt(sVal[1]);
+
+
+                    System.out.println( "Your point is: " + iValRow + "," + iValCol);
+
+                    int rowTest = cPointX.setPoint();
+                    int colTest = cPointY.setPoint();
+
+
+                    board[iValRow][iValCol] = " O";
+
+                    innerLoop:
+                    if ((!board[rowTest][colTest].equalsIgnoreCase("---")) || ((rowTest == iValRow) && (colTest == iValCol)))
                     {
-                        rowTest = cPointX.setPoint();
-                        colTest = cPointY.setPoint();
-                        if (board[rowTest][colTest].equals("---") && (rowTest == iValRow) && (colTest == iValCol))
+                        for (int k = 0; k < 10000; k++)
                         {
-                            board[rowTest][colTest] = " X";
-                            continue;
+                            rowTest = cPointX.setPoint();
+                            colTest = cPointY.setPoint();
+                            if (board[rowTest][colTest].equals("---") && ((rowTest != iValRow) && (colTest != iValCol)))
+                            {
+                                board[rowTest][colTest] = " X";
+                                break innerLoop;
+                            }
+
+
                         }
                     }
-                }
 
-                board[rowTest][colTest] = " X";
+                    System.out.println("The computer's point is: " + rowTest + "," + colTest);
+
+                    board[rowTest][colTest] = " X";
 
 
-                for (int row = 0; row < board.length; row++)
-                {
-                    for (int col = 0; col < board[row].length; col++)
+                    for (int row = 0; row < board.length; row++)
                     {
-                        System.out.print(board[row][col] + "\t");
+                        for (int col = 0; col < board[row].length; col++)
+                        {
+                            System.out.print(board[row][col] + "\t");
+                        }
+                        System.out.println("\n");
                     }
-                    System.out.println("\n");
-                }
 
-                break;
+                    if ((board[0][0].equalsIgnoreCase("O") && board[0][1].equalsIgnoreCase("O") && board[0][2].equalsIgnoreCase("O"))
+                            || (board[0][0].equalsIgnoreCase(" O") && board[0][1].equalsIgnoreCase(" O") && board[0][2].equalsIgnoreCase(" O"))
+                            || (board[1][0].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[1][2].equalsIgnoreCase(" O"))
+                            || (board[2][0].equalsIgnoreCase(" O") && board[2][1].equalsIgnoreCase(" O") && board[2][2].equalsIgnoreCase(" O"))
+                            || (board[0][0].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[2][2].equalsIgnoreCase(" O"))
+                            || (board[0][2].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[2][0].equalsIgnoreCase(" O"))
+                            || (board[0][0].equalsIgnoreCase(" O") && board[1][0].equalsIgnoreCase(" O") && board[2][0].equalsIgnoreCase(" O"))
+                            || (board[0][1].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[2][1].equalsIgnoreCase(" O"))
+                            || (board[0][2].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[2][0].equalsIgnoreCase(" O")))
+                    {
+                        System.out.println("The player wins!");
+                        System.out.println("Would you like to play again?");
+                        Scanner yn = new Scanner(System.in);
+                        String ans = yn.next();
+
+                        if (ans.equalsIgnoreCase("yes"))
+                        {
+                            break outerloop;
+                        }
+                        else
+                        {
+                            break outerloop1;
+                        }
+                    }
+
+
+                    else if ((board[0][0].equalsIgnoreCase(" X") && board[0][1].equalsIgnoreCase(" X") && board[0][2].equalsIgnoreCase(" X"))
+                            || (board[0][0].equalsIgnoreCase(" X") && board[0][1].equalsIgnoreCase(" X") && board[0][2].equalsIgnoreCase(" X"))
+                            || (board[1][0].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[1][2].equalsIgnoreCase(" X"))
+                            || (board[2][0].equalsIgnoreCase(" X") && board[2][1].equalsIgnoreCase(" X") && board[2][2].equalsIgnoreCase(" X"))
+                            || (board[0][0].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[2][2].equalsIgnoreCase(" X"))
+                            || (board[0][2].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[2][0].equalsIgnoreCase(" X"))
+                            || (board[0][0].equalsIgnoreCase(" X") && board[1][0].equalsIgnoreCase(" X") && board[2][0].equalsIgnoreCase(" X"))
+                            || (board[0][1].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[2][1].equalsIgnoreCase(" X"))
+                            || (board[0][2].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[2][0].equalsIgnoreCase(" X")))
+                    {
+                        System.out.println("The computer wins!");
+                        System.out.println("Would you like to play again?");
+                        Scanner yn = new Scanner(System.in);
+                        String ans = yn.next();
+
+                        if (ans.equalsIgnoreCase("yes"))
+                        {
+                            break outerloop;
+                        }
+                        else
+                        {
+                            break outerloop1;
+                        }
+
+                    }
+
+                }
 
             }
 
+
+            if (cp.equalsIgnoreCase("player"))
+            {
+
+                System.out.println("Player 1 will be O and Player 2 will be X. Player 1 goes first.");
+
+                for (int i = 0; i < 9; i++)
+                {
+
+                    System.out.println("Player 1, please enter a point");
+
+                    Scanner kI = new Scanner(System.in);
+                    String p1Choice = kI.next();
+
+                    System.out.println("Player 2, please enter a point");
+
+                    Scanner kI2 = new Scanner(System.in);
+                    String p2Choice = kI2.next();
+
+                    String sValP1[] = p1Choice.split(",");
+                    int iValRowP1 = Integer.parseInt(sValP1[0]);
+                    int iValColP1 = Integer.parseInt(sValP1[1]);
+
+                    String sValP2[] = p2Choice.split(",");
+                    int iValRowP2 = Integer.parseInt(sValP2[0]);
+                    int iValColP2 = Integer.parseInt(sValP2[1]);
+
+
+                    System.out.println("Player 1's point is: " + iValRowP1 + "," + iValColP1);
+                    System.out.println("Player 2's point is: " + iValRowP2 + "," + iValColP2);
+
+                    board[iValRowP1][iValColP1] = " O";
+                    board[iValRowP2][iValColP2] = " X";
+
+
+                    for (int row = 0; row < board.length; row++) {
+                        for (int col = 0; col < board[row].length; col++) {
+                            System.out.print(board[row][col] + "\t");
+                        }
+                        System.out.println("\n");
+                    }
+
+                    if ((board[0][0].equalsIgnoreCase("O") && board[0][1].equalsIgnoreCase("O") && board[0][2].equalsIgnoreCase("O"))
+                            || (board[0][0].equalsIgnoreCase(" O") && board[0][1].equalsIgnoreCase(" O") && board[0][2].equalsIgnoreCase(" O"))
+                            || (board[1][0].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[1][2].equalsIgnoreCase(" O"))
+                            || (board[2][0].equalsIgnoreCase(" O") && board[2][1].equalsIgnoreCase(" O") && board[2][2].equalsIgnoreCase(" O"))
+                            || (board[0][0].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[2][2].equalsIgnoreCase(" O"))
+                            || (board[0][2].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[2][0].equalsIgnoreCase(" O"))
+                            || (board[0][0].equalsIgnoreCase(" O") && board[1][0].equalsIgnoreCase(" O") && board[2][0].equalsIgnoreCase(" O"))
+                            || (board[0][1].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[2][1].equalsIgnoreCase(" O"))
+                            || (board[0][2].equalsIgnoreCase(" O") && board[1][1].equalsIgnoreCase(" O") && board[2][0].equalsIgnoreCase(" O")))
+                    {
+                        System.out.println("Player 1 wins!");
+                        break;
+                    }
+
+                    else if ((board[0][0].equalsIgnoreCase(" X") && board[0][1].equalsIgnoreCase(" X") && board[0][2].equalsIgnoreCase(" X"))
+                            || (board[0][0].equalsIgnoreCase(" X") && board[0][1].equalsIgnoreCase(" X") && board[0][2].equalsIgnoreCase(" X"))
+                            || (board[1][0].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[1][2].equalsIgnoreCase(" X"))
+                            || (board[2][0].equalsIgnoreCase(" X") && board[2][1].equalsIgnoreCase(" X") && board[2][2].equalsIgnoreCase(" X"))
+                            || (board[0][0].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[2][2].equalsIgnoreCase(" X"))
+                            || (board[0][2].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[2][0].equalsIgnoreCase(" X"))
+                            || (board[0][0].equalsIgnoreCase(" X") && board[1][0].equalsIgnoreCase(" X") && board[2][0].equalsIgnoreCase(" X"))
+                            || (board[0][1].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[2][1].equalsIgnoreCase(" X"))
+                            || (board[0][2].equalsIgnoreCase(" X") && board[1][1].equalsIgnoreCase(" X") && board[2][0].equalsIgnoreCase(" X")))
+                    {
+                        System.out.println("Player 2 wins!");
+                        break;
+                    }
+                }
+            }
         }
+
+
+
     }
 }
